5702ea1a09698359ddbea67a99dc935fe610ca04
Wady <wady.tupiza@gmail.com>
2017-05-27 21:15:51 -0500

    last minute changes

--- web/week06/activate.php
+++ web/week06/activate.php
@@ -1,3 +1,50 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Week 06</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="description" content="Demo project">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="../css/myStyle.css">
+	</head>
+	<body>
+		<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="../index.php">CS 313</a>
+				</div>
+				<div class="collapse navbar-collapse" id="myNavbar">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="../index.php">Home</a></li>
+						<li><a href="../assignments.html">My Assignments</a></li>
+						<li>
+							<a href="#"></a>
+						</li>
+						<li>
+							<a href="#"></a>
+						</li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<div class="main">
+		<div id="banner" class="jumbotron">
+			<h1>Database Week 06</h1>
+		</div>
+
+
 <?php
 require('includes/config.php');
 
@@ -28,3 +75,13 @@ if(is_numeric($memberID) && !empty($active)){
 	
 }
 ?>
+
+</div>
+
+
+	<footer class="col-md-12">
+    <p></p>
+        Copyright 2017
+    </footer>
+  </body>
+</html>
